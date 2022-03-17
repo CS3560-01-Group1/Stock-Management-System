943a00bdc0cc5bffd46049a16f7c5b2bcfd067ae
Joshua Cervantes <jecervantes15@gmail.com>
2022-03-17 15:19:45 -0700

    3rd comment

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 //This is a test to see if my git connected correctly 
 //Committing this should change something in main?
+//3rd comment appearing on Josh branch first, then merging to main to see if it updates.
 public class Main {
 
 	public static void main(String[] args) {
